076f443497706ef7741f6ec5c2208f1ace3390ff
Karl-Han.Ubuntu <karl-han@iwktd.com>
2018-10-24 14:55:34 +0800

    Rearrange the structure of file. Add the primitive factorial 1000 algorithm.

--- week8/Fatorial1000.c
+++ week8/Fatorial1000.c
@@ -5,35 +5,38 @@
 short digit[10000];
 int length = 0;
 
+/*
+ * TO DO
+ * 1. Improve the efficiency by decreasing the postion to multiply.
+ *
+ */
+
 void fatorial(int fac){
 	memset(digit,0,sizeof(short)*10000);
 	digit[0] = 1;
 	int carry = 0;
 	int sum = 0;
+	//int pos = 1;
 	for(int i = 2;i <= fac;i++){
 		;
+		//int newpos = 0;
 		for(int j = 0;j < 10000;j++){
-			if(digit[j] == 0){
-				digit[j] = carry;
-				//carry = 0;
-				//sum = 0;
-				continue;
-			}
 			sum = i * digit[j] + carry;
 			digit[j] = sum % 10;
 			carry = (sum - digit[j]) / 10;
 		}
 	}
 	length = 9999;
-	while(digit[length--] == 0);
-	printf("%d\n",length);
+	while(digit[length] == 0){
+		length--;
+	}
 }
 
 int main(){
 	int fac = 0;
 	scanf("%d",&fac);
 	fatorial(fac);
-	for(int i = length-1;i >= 0;i--){
+	for(int i = length;i >= 0;i--){
 		printf("%d",digit[i]);
 	}
 	return 0;
